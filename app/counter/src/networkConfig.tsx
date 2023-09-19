64e1b5c31d9ca2fef5f2935f0ef48eeee6b872b9
nsingal <31551081+nsingal@users.noreply.github.com>
2023-09-19 12:46:40 -0700

    Update networkConfig.tsx

--- app/counter/src/networkConfig.tsx
+++ app/counter/src/networkConfig.tsx
@@ -6,7 +6,7 @@ const networks: { [key: string]: { [key: string]: any } } = {
     accountPrivateKey: "ENTER PRIVATE KEY FOR TESTNET HERE",
   },
   'local': {
-    url: "http://127.0.0.1:8545",
+    url: "http://127.0.0.1:8545", // If you are running in VS Codespaces, please make the anvil port "PUBLIC" and update this to the public address
     chainId: 31337,
     accountAddress: "ENTER ACCOUNT ADDRESS FOR LOCAL NET HERE",
     accountPrivateKey: "ENTER PRIVATE KEY FOR LOCAL NET HERE",
